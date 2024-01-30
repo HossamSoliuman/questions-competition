587934f0ebb866c47eff0dbd115478ed80e25819
Hossam <hossamsoliuman@gmail.com>
2024-01-30 16:12:41 +0200

    prevent multipile device login for the same user

---
+++ app/Http/Middleware/CheckSingleDeviceLogin.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+
+class CheckSingleDeviceLogin
+{
+    public function handle($request, Closure $next)
+    {
+        $user = Auth::user();
+
+        // Check if the user is logged in
+        if ($user) {
+            $currentSessionId = Session::getId();
+
+            // Check if the current session ID matches the stored session ID
+            if ($user->session_id !== $currentSessionId) {
+                Auth::logout();
+                return redirect()->route('login')->with('error', 'You are logged in from another device.');
+            }
+        }
+
+        return $next($request);
+    }
+}
